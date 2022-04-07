40ae2a3642d8afe9e51da7a80ef65bff0b60c1cf
Flavio Macedo <fhenrique609@gmail.com>
2022-04-07 09:25:15 -0300

    Criando a página de login

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path('logar/', views.logar, name='logar'),
+    path('sair/', views.sair, name='sair'),
 ]
