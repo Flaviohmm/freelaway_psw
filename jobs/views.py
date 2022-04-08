65adbaf963394a2c3db04c3d2b157f959a0b687f
Flavio Macedo <fhenrique609@gmail.com>
2022-04-08 15:59:04 -0300

    Envio de projeto

--- jobs/views.py
+++ jobs/views.py
@@ -4,8 +4,10 @@ from .models import Jobs
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/auth/logar')
 def encontrar_jobs(request):
     if request.method == "GET":
         preco_minimo = request.GET.get('preco_minimo')
@@ -46,6 +48,7 @@ def encontrar_jobs(request):
         return render(request, 'encontrar_jobs.html', {'jobs': jobs})
 
 
+@login_required(login_url='/auth/logar')
 def aceitar_job(request, id):
     job = Jobs.objects.get(id=id)
     job.profissional = request.user
@@ -54,6 +57,7 @@ def aceitar_job(request, id):
     return redirect('/jobs/encontrar_jobs')
 
 
+@login_required(login_url='/auth/logar')
 def perfil(request):
     if request.method == "GET":
         jobs = Jobs.objects.filter(profissional=request.user)
@@ -84,3 +88,16 @@ def perfil(request):
 
         messages.add_message(request, constants.SUCCESS, 'Dados alterado com sucesso')
         return redirect('/jobs/perfil')
+
+
+@login_required(login_url='/auth/logar')
+def enviar_projeto(request):
+    arquivo = request.FILES.get('file')
+    id_job = request.POST.get('id')
+
+    job = Jobs.objects.get(id=id_job)
+
+    job.arquivo_final = arquivo
+    job.status = 'AA'
+    job.save()
+    return redirect('/jobs/perfil')
