12b4c0753d027990baff6677e5b7af820246656b
Flavio Macedo <fhenrique609@gmail.com>
2022-04-08 13:13:29 -0300

    Função de aceitar_job

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Jobs
 # Create your views here.
 
@@ -42,3 +42,11 @@ def encontrar_jobs(request):
             jobs = Jobs.objects.filter(reservado=False)
 
         return render(request, 'encontrar_jobs.html', {'jobs': jobs})
+
+
+def aceitar_job(request, id):
+    job = Jobs.objects.get(id=id)
+    job.profissional = request.user
+    job.reservado = True
+    job.save()
+    return redirect('/jobs/encontrar_jobs')
